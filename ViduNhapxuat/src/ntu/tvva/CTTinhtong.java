14d5884bb2b74ef77235e0c8243aa62df84ad12b
teddieV <vietanhtran013@gmail.com>
2025-02-20 04:14:09 +0700

    Update CTTINHTONG

--- ViduNhapxuat/src/ntu/tvva/CTTinhtong.java
+++ ViduNhapxuat/src/ntu/tvva/CTTinhtong.java
@@ -1,19 +1,29 @@
-/**
+/**dự án nhập xuất CT TINH TONG
  * 
  */
 package ntu.tvva;
+import java.util.Scanner;
 
 /**
  * 
  */
 public class CTTinhtong {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// In Ra Tiêu đề App
+		System.out.print("CT TINH TONG \n");
+		//Chuẩn bị cho việc nhập
+		Scanner banPhim = new Scanner(System.in);
+		//In dong huong nhan Moi Nhap A Va B
+		System.out.println(" Mời Nhập A");
+		double a= banPhim.nextDouble();
+		System.out.println(" Mời Nhập B");
+		double b= banPhim.nextDouble();		
+		//Tinh
+		double tong = a+b;
+		
+		System.out.print("Tổng Là: ");
+		System.out.print(tong);
 	}
 
 }
