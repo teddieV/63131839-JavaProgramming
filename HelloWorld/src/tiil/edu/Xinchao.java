a30c4c6c80f66af27280345fa0e7ecfcc9db16ad
teddieV <vietanhtran013@gmail.com>
2025-02-20 03:29:38 +0700

    Cap Nhat
    Them dong moi

--- HelloWorld/src/tiil/edu/Xinchao.java
+++ HelloWorld/src/tiil/edu/Xinchao.java
@@ -14,7 +14,7 @@ public class Xinchao {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print(" Lan Dau Trai Nghiem Git");
-
+		System.out.print("Lan dau trai nghiem Java");
 	}
 
 }
